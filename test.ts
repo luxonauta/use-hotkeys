de116f2e59d5eb6ca85e9121b001b71a1c911391
luxonauta <luxon4uta@gmail.com>
2025-09-16 06:16:37 -0300

    🚿 fix keywords & remove unused test file

--- test.ts
+++
@@ -1,6 +0,0 @@
-import { useHotkeys } from "./src/use-hotkeys";
-
-useHotkeys(["z", "y"], (e) => undoOrRedo(e), { requireMeta: true });
-const indicator = useHotkeys("k", () => openCommandPalette(), {
-  returnState: true
-});
